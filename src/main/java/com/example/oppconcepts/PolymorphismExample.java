df99a266bcf932cb0374626ece80dac9688a91f7
Victor Hugo <vhsilvat@gmail.com>
2023-11-16 16:41:58 -0300

    definição de polimorfismo e exemplos dos princípios SOLID

--- src/main/java/com/example/oppconcepts/PolymorphismExample.java
+++ src/main/java/com/example/oppconcepts/PolymorphismExample.java
@@ -3,101 +3,53 @@ package com.example.oppconcepts;
 public class PolymorphismExample {
 
 	/*
-	 * Polymorphism is the ability of an object, variable or method to take on many forms.
-	 * Runtime polymorphism is the ability of an object to take on many forms at
-	 * run time. It's linked with the concept of overriding.
-	 * Overriding is the process of defining a method in a subclass that provides
-	 * a specific implementation of the method declared in the superclass.
+	 * Polimorfismo é a capacidade de um objeto, variável ou método assumir muitas
+	 * formas. Em Java, polimorfismo é alcançado por meio overloading um método ou
+	 * overriding um método.
 	 *
+	 * Method overloading ocorre quando uma classe possui vários métodos com o mesmo
+	 * nome, mas com parâmetros (assinatura) diferente. O overloading acontece
+	 * durante RUN TIME.
+	 *
+	 * Method overriding ocorre quando uma subclasse fornece sua própria implementação
+	 * de um método já definido na superclasse. O overriding acontece durante o
+	 * compile time.
 	 * Compile time polymorphism is the ability of an object to take on many
 	 * forms at compile time. It's linked with the concept of OVERLOADING.
 	 */
 
-	static abstract class Shape {
-
-		int size;
-
-		void draw() {
-			System.out.println("Drawing a shape");
-		};
-
-		// OVERLOAD EXAMPLE
-		// The overloaded method not necessarily needs to be inherited.
-		void draw(int size) {
-			System.out.println("Drawing a shape with size " + size);
-		};
-
-
-		// If there is no implementation of the abstract method, then the
-		// compiler will throw an error.
-		// The abstract method MUST be implemented (overrided) in the subclasses.
-		abstract void paint();
-	}
-
-	// The object of the superclass Shape is constructed by the runtime environment.
-	static class Circle extends Shape { // Object to object inheritance
-
-		// Overriding the draw method from the superclass
-		@Override
-		void draw() {
-			System.out.println("Drawing a circle");
+	static class Animal {
+		void fazBarulho() {
+			System.out.println("O animal faz barulho.");
 		}
 
-		// Overriding and implementing the paint method from the superclass
-		@Override
-		void paint() {
-			System.out.println("Painting a circle");
+		// Overloading: mesmo nome, parâmetros diferentes (assinatura)
+		void fazBarulho(String sound) {
+			System.out.println("O animal faz o barulho: " + sound);
 		}
 	}
 
-	static class Rectangle extends Shape {
-
+	static class Cachorro extends Animal {
 		@Override
-		void draw() {
-			System.out.println("Drawing a rectangle");
+		void fazBarulho() {
+			System.out.println("O cachorro late.");
 		}
 
 		@Override
-		void paint() {
-			System.out.println("Painting a rectangle");
+			// Overriding: mesma assinatura, implementação diferente
+		void fazBarulho(String sound) {
+			System.out.println("O cachorro faz o barulho: " + sound);
 		}
 	}
 
 	public static void main(String[] args) {
+		Animal animal1 = new Cachorro();
+		Animal animal2 = new Animal();
 
-		Shape s;
-
-		/*
-		 * Example of runtime polymorphism:
-		 * Overridden methods "changes" the superclass method.
-		 * It must have the same signature as the superclass method.
-		 */
-
-		// Reference variable of type Shape, but it is an object of type Circle.
-		s = new Circle(); // This is called polymorphic statement.
-		s.draw();
-		s.paint();
-
-		// Reassign the reference variable "s" to reference an object of type Rectangle
-		// using the same method.
-		s = new Rectangle();
-		// Drawing different shapes dynamically at runtime.
-		s.draw(); // This is a runtime polymorphism, or upcasting.
-		s.paint();
-
-		/*
-		 * Example of compile time polymorphism.
-		 */
-
-		s = new Shape() {
-			@Override
-			void paint() {
-				System.out.println("Painting a shape");
-			}
-		};
-
-		s.draw();
-		s.draw(2);
-
+		animal1.fazBarulho();				// Output: O cachorro late.
+		animal1.fazBarulho("Woof!");	// Output: O cachorro faz o barulho: Woof!
+		animal2.fazBarulho();				// Output: O animal faz barulho.
+		animal2.fazBarulho("Meow!");	// Output: O animal faz o barulho: Meow!
 	}
+
 }
--- src/main/java/com/example/solid/SolidPrinciples.java
+++ src/main/java/com/example/solid/SolidPrinciples.java
@@ -4,16 +4,35 @@ public class SolidPrinciples {
 
 	/*
 	 * SOLID PRINCIPLES:
-	 *  - Single Responsibility Principle: A class should have only one reason to
-	 * change.
-	 * - Open/Closed Principle: A class should be open for extension, but closed
-	 * for modification.
-	 * - Liskov Substitution Principle: A class should be substitutable for its
-	 * subclasses.
-	 * - Interface Segregation Principle: A class should not have too many
-	 * dependencies on other classes.
-	 * - Dependency Inversion Principle: High-level modules should not depend on
-	 * low-level modules. Both should depend on abstractions.
+	 *
+	 * SOLID é um acrônimo que representa um conjunto de cinco princípios de design
+	 * para escrever códigos orientados a objetos de forma escalável. Cada letra
+	 * representa um desses princípios:
+	 *
+	 * S - Princípio da Responsabilidade Única (SRP): Uma classe deve ter apenas uma
+	 * razão para mudar, ou seja, deve ter uma única responsabilidade ou tarefa.
+	 *
+	 * O - Princípio Aberto/Fechado (OCP): Entidades (classes, módulos, funções)
+	 * devem estar abertas para extensão, mas fechadas para modificação. Isso
+	 * promove o uso de herança, interfaces ou classes abstratas para adicionar
+	 * nova funcionalidade em vez de modificar código previamente existente.
+	 *
+	 * L - Princípio da Substituição de Liskov (LSP): Subclasses devem substituir
+	 * suas superclasses sem afetar a correção do programa. Em outras palavras, se
+	 * uma classe é subtipo de outra, ela deve ser utilizável no lugar da superclasse
+	 * sem problemas.
+	 *
+	 * I - Princípio da Segregação de Interfaces (ISP): Clientes não devem ser forçados
+	 * a depender de interfaces que não utilizam. Esse princípio promove a criação de
+	 * interfaces específicas e focadas em vez de interfaces grandes e monolíticas.
+	 *
+	 * D - Princípio da Inversão de Dependência (DIP): Módulos de alto nível não
+	 * devem depender de módulos de baixo nível. Ambos devem depender de abstrações.
+	 * Esse princípio incentiva o uso de injeção de dependência e inversão de
+	 * controle para desacoplar componentes.
+	 *
+	 * Estes princípios visam melhorar a manutenibilidade, flexibilidade e robustez
+	 * do código, promovendo boas práticas de design orientados a objetos.
 	 *
 	 *
 	 * - Principle of Least Knowledge: A class should only depend on other classes
@@ -23,4 +42,146 @@ public class SolidPrinciples {
 	 * - Principle of Least Codding: A class should only depend on other classes
 	 * that it uses.
 	 */
+
+	// EXEMPLOS:
+
+	/* S - Princípio da Responsabilidade Única (SRP): Uma classe deve ter apenas uma
+	razão para mudar, ou seja, deve ter uma única responsabilidade ou tarefa. */
+	// Antes da Responsabilidade Única
+	static class Relatorios {
+		void gerar() { /* gera relatório */ }
+		void salvarEmArquivo() { /* salva relatório em arquivo */ }
+	}
+	// Depois da Responsabilidade Única
+	static class Relatorio {
+		void gerar() { /* gera relatório */ }
+	}
+	static class SalvaRelatorio {
+		void salvarEmArquivo(Relatorio relatorio) { /* salva relatório em arquivo */ }
+	}
+
+
+	/* O - Princípio Aberto/Fechado (OCP): Entidades (classes, módulos, funções)
+	devem estar abertas para extensão, mas fechadas para modificação. Isso promove
+	o uso de herança, interfaces ou classes abstratas para adicionar nova
+	funcionalidade em vez de modificar código previamente existente. */
+	// Antes do Aberto/Fechado
+	static class Forma {
+		void desenho() { /* desenha forma */ }
+	}
+	static class Circulo extends Forma {
+		void desenho() { /* desenha círculo */ }
+	}
+
+	// Depois do Aberto/Fechado
+	interface Formas {
+		void desenhar();
+	}
+	static class Circulos implements Formas {
+		@Override
+		public void desenhar() { /* desenha circulo */ }
+	}
+	static class Quadrados implements Formas {
+		@Override
+		public void desenhar() { /* desenha quadrado */ }
+	}
+
+
+	/* L - Princípio da Substituição de Liskov (LSP): Subclasses devem substituir
+	suas superclasses sem afetar a correção do programa. Em outras palavras, se
+	uma classe é subtipo de outra, ela deve ser utilizável no lugar da superclasse
+	sem problemas. */
+	// Violação da Substituição de Liskov
+	static class Passaro {
+		void voar() { /* voa */ }
+	}
+	static class Galinha extends Passaro {
+		void voar() { /* incapaz de voar */ }
+	}
+	// Adesão à Substituição de Liskov
+	interface Ave {
+		void mover();
+	}
+	static class Pardal implements Ave {
+		@Override
+		public void mover() { /* voa */ }
+	}
+	static class Pinguim implements Ave {
+		@Override
+		public void mover() { /* nada */ }
+	}
+
+
+	/* I - Princípio da Segregação de Interfaces (ISP): Clientes não devem ser forçados
+	a depender de interfaces que não utilizam. Esse princípio promove a criação de
+	interfaces específicas e focadas em vez de interfaces grandes e monolíticas. */
+	// Violação da Segregação de Interfaces
+	interface Trabalhador {
+		void trabalhar();
+		void fazerIntervalo();
+	}
+	static class Pedreiro implements Trabalhador {
+		@Override
+		public void trabalhar() { /* trabalho de engenharia */ }
+		@Override
+		public void fazerIntervalo() { /* faz um intervalo */ }
+	}
+	// Adesão à Segregação de Interfaces
+	interface Funcionario {
+		void trabalhar();
+	}
+	interface Intervalos {
+		void fazerIntervalo();
+	}
+	class Engenheiro implements Funcionario, Intervalos {
+		@Override
+		public void trabalhar() { /* trabalho de engenharia */ }
+		@Override
+		public void fazerIntervalo() { /* faz um intervalo */ }
+	}
+
+
+	/* D - Princípio da Inversão de Dependência (DIP): Módulos de alto nível não
+	devem depender de módulos de baixo nível. Ambos devem depender de abstrações.
+	Esse princípio incentiva o uso de injeção de dependência e inversão de
+	controle para desacoplar componentes. */
+	// Sem Inversão de Dependência
+	static class Lampada {
+		void ligar() { /* liga luz */ }
+		void desligar() { /* desliga luz */ }
+	}
+	class Interruptor {
+		Lampada lampada = new Lampada();
+		void operar() {
+			if (true /* condição */) {
+				lampada.ligar();
+			} else {
+				lampada.desligar();
+			}
+		}
+	}
+	// Com Inversão de Dependência
+	interface DispositivoLigavel {
+		void ligar();
+		void desligar();
+	}
+	class Luminaria implements DispositivoLigavel {
+		@Override
+		public void ligar() { /* liga luz */ }
+		@Override
+		public void desligar() { /* desliga luz */ }
+	}
+	class Interruptor1 {
+		DispositivoLigavel dispositivo;
+		Interruptor1(DispositivoLigavel dispositivo) {
+			this.dispositivo = dispositivo;
+		}
+		void operar() {
+			if (true /* condição */) {
+				dispositivo.ligar();
+			} else {
+				dispositivo.desligar();
+			}
+		}
+	}
 }
