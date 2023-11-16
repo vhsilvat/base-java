a6f9dfcd6f75865c7026d4c578bc87603134f49a
Victor Hugo <vhsilvat@gmail.com>
2023-11-16 11:49:36 -0300

    definição de encapsulamento

--- src/main/java/com/example/oppconcepts/DiamondProblem.java
+++ src/main/java/com/example/oppconcepts/DiamondProblem.java
@@ -3,16 +3,16 @@ package com.example.oppconcepts;
 public class DiamondProblem {
 
 	/*
-	 * The diamond problem is a situation in which a class has two or more
-	 * interfaces that it implements. The class must implement all the methods
-	 * defined in the interfaces.
+	 * O problema diamante é uma situação em que uma classe possui duas ou mais
+	 * interfaces implementadas. A classe deve implementar todos os métodos
+	 * definidos nas interfaces.
 	 *
-	 * It comes with the conflit caused by multiple interfaces having the same
-	 * method name and signature.
+	 * O conflito ocorre quando múltiplas interfaces possuem os mesmos nomes de
+	 * métodos com as mesmas assinaturas.
 	 *
-	 * DEFAULT METHODS ALLOWS THE ADDITION OF NEW METHODS TO AN INTERFACE 
-	 * WITHOUT CHANGING CLASSES THAT IMPLEMENTS THE INTERFACE. This, in turn,
-	 * can cause the diamond problem too.
+	 * Métodos default permitem a adição de novos métodos em uma interface sem
+	 * alterar as classes que a implementam. Isso, por sua vez, também pode causar
+	 * o problema diamante.
 	 *
 	 * Whenever we need to add functionality to a legacy interface, we can
 	 * use default methods without breaking any implementor classes;
@@ -35,8 +35,8 @@ public class DiamondProblem {
 	public static class class01 implements interface01, interface02 {
 
 		/*
-		 * We can explicitly implement all the conflicting methods
-		 * defined in the interfaces.
+		 * Podemos implementar explicitamente todos os métodos conflitantes definidos
+		 * nas interfaces
 		 */
 //		@Override
 //		public void defMethod() {
@@ -44,7 +44,7 @@ public class DiamondProblem {
 //		}
 
 		/*
-		 * We can explicitly invoke the default method from the interface
+		 * Podemos invocar explicitamente o método padrão da interface
 		 */
 //		@Override
 //		public void defMethod() {
@@ -52,18 +52,13 @@ public class DiamondProblem {
 //		}
 
 		/*
-		 * Futhermore, we can also invoke both the default methods
+		 * Além disso, podemos também invocar ambos os métodos default ao mesmo tempo
 		 */
-
 		@Override
 		public void defMethod() {
 			interface01.super.defMethod();
 			interface02.super.defMethod();
 		}
-
-		/*
-		 * We can also invoke the default method from the superclass
-		 */
 	}
 
 	public static void main(String[] args) {
--- src/main/java/com/example/oppconcepts/EncapsulationExample.java
+++ src/main/java/com/example/oppconcepts/EncapsulationExample.java
@@ -3,16 +3,33 @@ package com.example.oppconcepts;
 public class EncapsulationExample {
 
 	/*
-	 * Encapsulation is the process of wrapping the data and methods together
-	 * to hide the implementation details.
+	 * Encapsulamento é o processo de agrupar os dados e métodos para ocultar os
+	 * dados armazenados em classes. Isso ajuda a evitar que os dados sejam acessados
+	 * ou modificados por outras classes de maneira indesejada. O encapsulamento é
+	 * obtido através do uso de modificadores de acesso
 	 *
-	 * Encapsulation is achieved by:
-	 * 		- Making the fields (parameters) private
-	 * 		- Making the getters and setters public
-	 * 		- Making the constructors private
-	 * 		- Making the methods private
-	 *
-	 * Essentially, encapsulation provides a protective shield that prevents
-	 * data from being accessed by any code that exists outside the shield.
+	 * 		- Tornando os campos (parâmetros) privados;
+	 * 		- Tornando públicos os métodos getters e setters;
+	 * 		- Tornando os métodos privados
 	 */
+
+	public static class Pessoa {
+		private final String nome;
+		private final int idade;
+
+		public Pessoa(String nome, int idade) {
+			this.nome = nome;
+			this.idade = idade;
+		}
+
+		public String getNome() { return nome; }
+		public int getIdade() { return idade; }
+	}
+
+	public static void main(String[] args) {
+		Pessoa person = new Pessoa("John Doe", 30);
+		System.out.println("Nome: " + person.getNome());
+		System.out.println("Idade: " + person.getIdade());
+	}
+
 }
