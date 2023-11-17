9ebb9a3e4e79f9bcf60b51af5a9184d6b5916431
Victor Hugo <vhsilvat@gmail.com>
2023-11-17 16:51:50 -0300

    definições dos tipos de handlers do spring

---
+++ src/main/java/com/example/spring/HandlersSpring.java
@@ -0,0 +1,40 @@
+package com.example.spring;
+
+public class HandlersSpring {
+
+    /*
+     * HANDLER METHOD ARGUMENT RESOLVER
+     * É responsável por personalizar a resolução de methods arguments com anotações
+     * específicas ou lógica personalizada.
+     *
+     * Permite a resolução de methods arguments de controllers específicos, geralmente
+     * onde são anotados com alguma anotação específica. Oferece controle refinado
+     * sobre methods arguments de controladores específicos.
+     *
+     * É utilizado quando precisar personalizar resoluções de argumentos para controllers
+     * específico, especialmente quando houver uma lógica única ou personalizada a ser
+     * aplicada.
+     *
+     *
+     * HANDLER INTERCEPTOR
+     * Pode interceptar TODAS as solicitações recebidas em TODOS os controladores,
+     * independentemente dos method arguments.
+     *
+     * Fornece uma maneira de aplicar tratamentos transversais ou realizar tarefas como
+     * registro, autenticação, definição de atributos de modelos comuns ou manipulação
+     * de lógica de pós e pré-processamento para um amplo conjunto de controladores.
+     *
+     * Funciona num nível mais alto no fluxo de processamento de solicitações e não
+     * está vinculado a method arguments específicos.
+     *
+     * Usado quando necessário aplicar lógica comum a vários controladores ou quando
+     * precisar interceptar e realizar ações em solicitações em diferentes estágios
+     * do ciclo de vida do processamento de requests HTTP, independentemente dos method
+     * arguments.
+     */
+
+
+
+
+
+}
